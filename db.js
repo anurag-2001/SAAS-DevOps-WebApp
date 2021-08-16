11dbdc28b197e93fbd7997d1c3a87ae34c075690
keshav dhomase <keshavzm@gmail.com>
2021-08-16 15:11:10 +0530

    Keshav Part

--- db.js
+++ db.js
@@ -2,8 +2,8 @@ const dotenv = require('dotenv')
 dotenv.config()
 const mongodb = require('mongodb')
 
-mongodb.connect(process.env.CONNECTIONSTRING,{userNewUrlParser: true, useUnifiedTopology: true},function(err, client){
-    module.exports = client
-    const app = require('./app')
-    app.listen(process.env.PORT)
+mongodb.connect(process.env.CONNECTIONSTRING, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client) {
+  module.exports = client
+  const app = require('./app')
+  app.listen(process.env.PORT)
 })
