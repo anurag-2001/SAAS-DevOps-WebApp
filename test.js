8ef729ee3581ac5b25af97cb77b9168c2c1eaf06
Anurag Sharma <76194838+anurag-2001@users.noreply.github.com>
2021-10-19 17:33:03 +0530

    update

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var userController = require('./controllers/userController');
+const userController = require('./controllers/userController');
 var postController = require('./controllers/postController');
 var followController = require('./controllers/followController');
 
