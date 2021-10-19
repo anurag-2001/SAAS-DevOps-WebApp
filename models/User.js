7b3a7cbf1cbb9339aa277070bc2701213fe04769
Anurag Sharma <76194838+anurag-2001@users.noreply.github.com>
2021-10-19 17:20:52 +0530

    Update password

--- models/User.js
+++ models/User.js
@@ -29,7 +29,7 @@ User.prototype.validate = function() {
     if (this.data.username != "" && !validator.isAlphanumeric(this.data.username)) {this.errors.push("Username can only contain letters and numbers.")}
     if (!validator.isEmail(this.data.email)) {this.errors.push("You must provide a valid email address.")}
     if (this.data.password == "") {this.errors.push("You must provide a password.")}
-    if (this.data.password.length > 0 && this.data.password.length < 12) {this.errors.push("Password must be at least 12 characters.")}
+    if (this.data.password.length > 0 && this.data.password.length < 8) {this.errors.push("Password must be at least 8 characters.")}
     if (this.data.password.length > 50) {this.errors.push("Password cannot exceed 50 characters.")}
     if (this.data.username.length > 0 && this.data.username.length < 3) {this.errors.push("Username must be at least 3 characters.")}
     if (this.data.username.length > 30) {this.errors.push("Username cannot exceed 30 characters.")}
